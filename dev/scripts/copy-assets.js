4a776cdc18dee07e44ca7aa68b00d91158678b5c
Sergeev Nick <sergeev.nickitos@yandex.ru>
2020-02-10 14:14:22 +0700

    Update copy-assets.js

--- dev/scripts/copy-assets.js
+++ dev/scripts/copy-assets.js
@@ -2,4 +2,4 @@ const { copy } = require('@eigenspace/helper-scripts');
 
 const target = 'dist';
 
-copy(['package.json', 'yarn.lock', 'src/assets'], target);
+copy(['package.json', 'yarn.lock'], target);
