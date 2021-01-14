88358141cf39ef2c606a90e38bc7c7b727f039c9
Nikita Agupov <nktrsk@gmail.com>
2021-01-14 17:04:19 +0300

    microfix common: fixed critical files

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    apiUrls: {
+        base: 'https://atlassian.net/rest/api/3',
+        issues: {
+            base: '/issue',
+            getIssue: '/:issueIdOrKey',
+            editIssue: '/:issueIdOrKey'
+        }
+    }
+};
